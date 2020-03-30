8ecbabcabb71927bbf0673e3075d4a023dec8cf1
Anessy <n.repekha@gmail.com>
2020-03-30 16:27:03 +0300

    5 lesson

--- JS/js/script.js
+++ JS/js/script.js
@@ -24,6 +24,22 @@ function showFun3() {
   }
 }
 
+function showFun4() {
+  if(document.getElementById('myShowBlock4' ).style.display == 'none') {
+      document.getElementById('myShowBlock4' ).style.display = 'block';
+  } else {
+      document.getElementById('myShowBlock4' ).style.display = 'none';
+  }
+}
+
+function showFun5() {
+  if(document.getElementById('myShowBlock5' ).style.display == 'none') {
+      document.getElementById('myShowBlock5' ).style.display = 'block';
+  } else {
+      document.getElementById('myShowBlock5' ).style.display = 'none';
+  }
+}
+
 // ================= ЧЕКБОКСЫ ==========================
 
 // получили чекбоксы и вывод результата
@@ -73,8 +89,142 @@ let downList = document.querySelector('#downList');
 
 // ставим слушатель события на "уход" мышки с элемента (mouseout)
 downList.addEventListener("mouseout", function(){
-  
+
 // при срабатывании ф-ции выводим в наш результат полученный с помощью selectedIndex номер выбранного элемента  
   document.querySelector('#result3').innerHTML = ("Вы выбрали " + downList.selectedIndex + " вариант"  );
 });
+
+// ======== input с вариантами от пользователя ===============
+
+// получаем кнопку
+let inputLesson4,
+    btn4 = document.querySelector('.btn4');
+
+    //поставим слушатель на кнопку - при нажатии, запустим ф-цию
+  btn4.addEventListener('click', function(){
+    // присвоим переменной inputLesson4 - введенное в input значение
+    inputLesson4 = document.querySelector('#inputLesson4').value;
+    // все буквы строки inputLesson4 переведем в нижний регистр 
+    inputLesson4 = inputLesson4.toLowerCase();
+    // split(', ') - разбивает строку на массив по указанному разделителю (в нашем случае это ', ');
+    inputLesson4 = inputLesson4.split(',');
+
+    document.querySelector('#result4').innerHTML = '';
+    // перебираем эллементы массива 
+    for (let i=0; i < inputLesson4.length; i++) {
+    //удаляем лишние проболы 
+    inputLesson4[i] = inputLesson4[i].trim(); // <= удаляем лишние пробелы <=
+    console.log('inputLesson4[i]: ', inputLesson4[i]);
+    
+    //  document.querySelector('#result4' -- получили наш результат
+    // innerHTML -- выводим в html 
+    // += -- добавляя к тому, что уже было записано в нем ранее
+    document.querySelector('#result4').innerHTML += '<br>' + '  Вы выбрали № ' + (+i+1) + ' = ' + inputLesson4[i] ;
+   
+  }
+  });
+
+  // ========== ASCII ===== unicode =======
+
+  // console.log('\xAE'); 
+  // console.log('\u00A9'); 
+  // console.log('🦊'); 
+  // let str = '='
+  // console.log(str.repeat(10));
+
+  // ============= дневной бюджет ================================================== 
+
+  let money;
+  document.querySelector('#buttonMoney').addEventListener('click',function(){
+    money = +document.querySelector('#inputMoney').value;
+    // ========= ========== ========= =========== ========== =========  проверка на число
+    document.querySelector('#moneyHtml').innerHTML = correct(money);
+    showType(money); 
+  }); 
+
+  let inputMainExpensesText, inputMainExpenses; 
+  document.querySelector('#buttonExpenses').addEventListener('click',function(){
+    inputMainExpensesText = document.querySelector('#inputMainExpensesText').value; 
+    inputMainExpenses = +document.querySelector('#inputMainExpenses').value; 
+    document.querySelector('#mainExpensesTetx').innerHTML = inputMainExpensesText;
+     // ========= ========== ========= =========== ==========  проверка на число
+    document.querySelector('#mainExpenses').innerHTML = correct(inputMainExpenses);
+    showType(inputMainExpenses); 
+  });
+
+  let inputSecondExpensesText, inputSecondExpenses; 
+  document.querySelector('#buttonSecondExpenses').addEventListener('click', function(){
+    inputSecondExpensesText = document.querySelector('#inputSecondExpensesText').value;
+    inputSecondExpenses = +document.querySelector('#inputSecondExpenses').value; 
+    document.querySelector('#secondExpensesText').innerHTML = inputSecondExpensesText;
+    // ========= ========== ========= =========== ==========  проверка на число
+    document.querySelector('#secondExpenses').innerHTML = correct(inputSecondExpenses);
+    showType(inputSecondExpenses); 
+    budget(); // ===================================== вычисление месячного и дневного бюджета 
+  });
+
+  // ф-ция проверки типа данных используем typeOf
+  let showType = function(data) {
+    console.log('Тип данных ', data,' = ' , typeof(data));
+  }
+
+  // проверка на корректность введения числа 
+  // (мы получаем NaN. если вводимые данные не являются числом)
+  //isNaN проверяет это и в случае правдивости выводит сообщение в результат
+
+let correct = function(a) {
+  if (isNaN(a)) {
+    return a = 'Ваш ответ не корректен. Введите число.'
+  } else {
+    return a;
+  }
+};
+
+let budget = function() {
+  // месячный бюджет 
+ let budgetMounth = money - (inputMainExpenses + inputSecondExpenses);
+ document.querySelector('#budgetMonth').innerHTML = budgetMounth;
+
+ // Дневной бюджет (Math.round - округление до ближайшего целого числа)
+ document.querySelector('#budgetDay').innerHTML = Math.round(budgetMounth / 30);
+};
+
+
+
+
+  
+
+
+
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+  
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
